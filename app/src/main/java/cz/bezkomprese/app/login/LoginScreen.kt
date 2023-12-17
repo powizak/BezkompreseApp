4084994151cf8ed66867f6b7321aa14338b02152
skalajan <jan.skala566@gmail.com>
2023-12-17 20:53:05 +0100

    feature(login) - init

--- app/src/main/java/cz/bezkomprese/app/login/LoginScreen.kt
+++ app/src/main/java/cz/bezkomprese/app/login/LoginScreen.kt
@@ -1,12 +1,57 @@
 package cz.bezkomprese.app.login
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.lifecycle.viewmodel.compose.viewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginScreen(){
+fun LoginScreen() {
     val viewModel = viewModel(LoginViewModel::class.java)
 
-    Text(text = "Login")
+
+    Column {
+
+
+        OutlinedTextField(
+            label = { Text(text = "Login") },
+            placeholder = { Text(text = "Přezdívka") },
+            value = viewModel.email.collectAsState().value,
+            supportingText = {
+                Text(text = "Zadejte Vaši přezdívku nebo email")
+            },
+            onValueChange = {
+                viewModel.email.value = it
+            })
+
+        OutlinedTextField(
+            label = { Text(text = "Heslo") },
+            placeholder = { Text(text = "Heslo") },
+            value = viewModel.password.collectAsState().value,
+            supportingText = {
+                Text(text = "Zadejte Vaše heslo")
+            },
+            onValueChange = {
+                viewModel.password.value = it
+            },
+            visualTransformation = PasswordVisualTransformation()
+        )
+        val error = viewModel.error.collectAsState().value
+        if(error != null) {
+            Text(text = error)
+        }
+        Button(onClick = { viewModel.loginClicked() }) {
+            Text(text = "Přihlásit")
+        }
+        OutlinedButton(onClick = { /*TODO*/ }) {
+            Text(text = "Registrovat")
+        }
+    }
 }
--- app/src/main/java/cz/bezkomprese/app/login/LoginViewModel.kt
+++ app/src/main/java/cz/bezkomprese/app/login/LoginViewModel.kt
@@ -1,7 +1,28 @@
 package cz.bezkomprese.app.login
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class LoginViewModel: ViewModel() {
+    val email = MutableStateFlow("")
+    val password = MutableStateFlow("")
 
+    val error = MutableStateFlow<String?>(null)
+
+
+    fun loginClicked(){
+        error.value = null
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+            email.value, password.value
+        ).apply {
+            addOnCompleteListener {
+                if(it.isSuccessful){
+                    // TODO
+                }else{
+                    error.value = it.exception?.toString() ?: ""
+                }
+            }
+        }
+    }
 }
